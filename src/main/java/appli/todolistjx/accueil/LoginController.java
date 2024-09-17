250f4a0cdd875656fd0835ad09d201e4d6cbbb68
Raphael Cazin <44144450+Javarwy@users.noreply.github.com>
2024-09-17 15:21:40 +0200

    (Mise à jour : 15h21)

--- src/main/java/appli/todolistjx/accueil/LoginController.java
+++ src/main/java/appli/todolistjx/accueil/LoginController.java
@@ -6,8 +6,12 @@ import javafx.scene.control.Label;
 public class LoginController {
     @FXML
     private Label welcomeText;
+    private Button welcomeText;
 
     @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
